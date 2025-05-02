20e7e8fac40fcf626630b054cebcf4a1e59fead0
JVCA101 <joaovictordealmeid@gmail.com>
2025-05-02 09:13:26 -0300

    finished

--- User.h
+++ User.h
@@ -6,6 +6,7 @@
 #include <vector>
 #include <algorithm>
 
+// the lower the number, the higher the priority
 enum class Role
 {
     GameMaster=0, // have access to write, roll dice and include/exclude player
@@ -20,6 +21,20 @@ private:
     std::string email{};
     Role role{};
     std::unique_ptr<std::vector<std::string>> list_of_players{nullptr};
+
+    /**
+     * * @brief Function to authenticate the user
+     * * @details This function checks if the user has the required role to perform an action.
+     * * @param required_role Minimal role required to perform the action. Default is GameMaster.
+     * * @return true if the user has the required role, false otherwise.
+     */
+    bool authenticate(Role required_role = Role::GameMaster)
+    {
+        if(this->role <= required_role)
+            return true;
+        else
+            return false;
+    }
     
 public:
     std::string name{};
@@ -30,43 +45,98 @@ public:
             list_of_players = std::make_unique<std::vector<std::string>>();
     }
 
-    void write(std::string text)
+    /**
+     * * @brief Function to write a message
+     * * @details This function allows the user to write a message. The message must be between 0 and 100 characters. Everyone can write.
+     */
+    void write()
     {
+        // Authenticate the user using RBAC
         if(!authenticate(Role::Espectator)){
             std::cout << "Error: User not authenticated.\n";
             return;
         }
-        std::cout << text << '\n';
+        std::string text;
+        std::cout << "Write text between 1 and 100 characters: ";
+        std::cin.ignore();
+        std::getline(std::cin, text);
+
+        // Check if the text is empty or exceeds 100 characters
+        if(text.empty()){
+            std::cout << "Error: Text cannot be empty.\n";
+            return;
+        }
+        if(text.length() > 100){
+            std::cout << "Error: Text length exceeds 100 characters.\n";
+            return;
+        }
+
+        // Print the message
+        std::cout << this->name << ": " << text << '\n';
     }
 
-    void rollDice(unsigned short num_sides)
+    /**
+     * * @brief Function to roll a dice
+     * * @details This function allows the user to roll a dice. The number of sides must be greater than 0 and lower than 100. Espectators cannot roll the dice.
+     */
+    void rollDice()
     {
+        // Authenticate the user using RBAC
         if(!authenticate(Role::Player)){
             std::cout << "Error: User not authenticated.\n";
             return;
         }
-        unsigned short num = (std::rand() % (num_sides-1)) + 1;
+
+        // User input for number of sides
+        short num_sides;
+        std::cout << "Enter number of sides for the dice (2-100): ";
+        std::cin >> num_sides;
+
+        // Check if the number of sides is valid
+        if(num_sides <= 1 || num_sides > 100){
+            std::cout << "Error: Number of sides must be between 2 and 100.\n";
+            return;
+        }
+
+        // Randomize the number
+        short num = (std::rand() % (num_sides-1)) + 1;
         std::cout << "Rolled a d" << num_sides << " and got: " << num << '\n';
     }
 
-    void include_player(std::string new_player)
+    /**
+     * * @brief Function to include a player
+     * * @details This function allows the GameMaster to include a player in the game. The player must not be already in the game.
+     */
+    void include_player()
     {
+        // Authenticate the user using RBAC
         if(!authenticate(Role::GameMaster)){
             std::cout << "Error: User not authenticated.\n";
             return;
         }
 
+        // Check if the list of players is null
         if(list_of_players == nullptr){
             std::cout << "Error: list_of_players is null.\n";
             return;
         }
 
+        // User input for new player name
+        std::string new_player;
+        std::cout << "Enter player name to include: ";
+        std::cin.ignore();
+        std::getline(std::cin, new_player);
+
+        // Check if the new player name is empty
         if(new_player.empty()){
             std::cout << "Error: Player name cannot be empty.\n";
             return;
         }
 
+        // Check if the new player is already in the game
+        // if the player is not in the game, it will return an iterator to the end of the vector
         auto it = std::find(list_of_players->begin(), list_of_players->end(), new_player);
+        
         if(it == list_of_players->end()){
             list_of_players->push_back(new_player);
             std::cout << new_player << " included in the game.\n";
@@ -75,19 +145,40 @@ public:
             std::cout << new_player << " is already in the game.\n";
     }
 
-    void exclude_player(std::string player)
+    /**
+     * * @brief Function to exclude a player
+     * * @details This function allows the GameMaster to exclude a player from the game. The player must be already in the game.
+     */
+    void exclude_player()
     {
+        // Authenticate the user using RBAC
         if(!authenticate(Role::GameMaster)){
             std::cout << "Error: User not authenticated.\n";
             return;
         }
 
+        // Check if the list of players is null
         if(list_of_players == nullptr){
             std::cout << "Error: list_of_players is null.\n";
             return;
         }
 
+        // User input for player name to exclude
+        std::string player;
+        std::cout << "Enter player name to exclude: ";
+        std::cin.ignore();
+        std::getline(std::cin, player);
+
+        // Check if the player name is empty
+        if(player.empty()){
+            std::cout << "Error: Player name cannot be empty.\n";
+            return;
+        }
+
+        // Check if the player is in the game
+        // if the player is not in the game, it will return an iterator to the end of the vector
         auto it = std::find(list_of_players->begin(), list_of_players->end(), player);
+
         if(it != list_of_players->end()){
             list_of_players->erase(it);
             std::cout << player << " excluded from the game.\n";
@@ -96,28 +187,33 @@ public:
             std::cout << player << " is not in the game.\n";
     }
 
+    /**
+     * * @brief Function to show the list of players
+     * * @details This function shows the list of players in the game. The GameMaster can see the list of players.
+     */
     void show_list_of_players()
     {
+        // Authenticate the user using RBAC
         if(!authenticate(Role::GameMaster)){
             std::cout << "Error: User not authenticated.\n";
             return;
         }
 
+        // Check if the list of players is null
         if(list_of_players == nullptr){
             std::cout << "Error: list_of_players is null.\n";
             return;
         }
 
+        // Check if the list of players is empty
+        if(list_of_players->empty()){
+            std::cout << "Error: list_of_players is empty.\n";
+            return;
+        }
+
+        // Print the list of players
         std::cout << "List of players:\n";
         for(const auto& player : *list_of_players)
             std::cout << player << '\n';
-    }
-
-    bool authenticate(Role required_role = Role::GameMaster)
-    {
-        if(this->role <= required_role)
-            return true;
-        else
-            return false;
     }
 };
--- rbac.cc
+++ rbac.cc
@@ -6,52 +6,49 @@ void choose_user(User*& currentUser, User& gameMaster, User& player, User& espec
 
 int main()
 {
+    // Initialize users for example
     User gameMaster{"Jao", "1@3$J", "jao@mail.com", Role::GameMaster};
     User player{"Gabriela", "1@3$G", "gabriela@mail.com", Role::Player};
     User espectator{"Sara", "1@3$S", "sara@mail.com", Role::Espectator};
     User* currentUser = nullptr;
     
     unsigned short command;
-    std::string text, new_player;
     while(true)
     {
-        choose_user(currentUser, gameMaster, player, espectator);
+        // If currentUser is nullptr, prompt to choose a user
+        if(currentUser == nullptr)
+            choose_user(currentUser, gameMaster, player, espectator);
+        
+        // If currentUser persists as nullptr, skip the loop
         if(currentUser == nullptr)
             continue;
         
-        std::cout << "Enter a command: \n(1) Write\n(2) Roll Dice\n(3) Show List of Players\n(4) Include Player\n" << 
-        "(5) Exclude Player\n(6) Change User\n(7) Exit\n";
+        std::cout << "\nEnter a command: " << 
+        "\n(1) Write"                    << 
+        "\n(2) Roll Dice"                << 
+        "\n(3) Show List of Players"     << 
+        "\n(4) Include Player\n"         << 
+        "(5) Exclude Player"             << 
+        "\n(6) Change User"              << 
+        "\n(7) Exit\n\n";
         std::cin >> command;
         
-        text.clear();
-        new_player.clear();
         switch(command)
         {
         case 1:
-            std::cout << "Enter text to write: ";
-            std::cin.ignore();
-            std::getline(std::cin, text);
-            currentUser->write(text);
+            currentUser->write();
             break;
         case 2:
-            std::cout << "Enter number of sides for the dice: ";
-            unsigned short num_sides;
-            std::cin >> num_sides;
-            currentUser->rollDice(num_sides);
+            currentUser->rollDice();
             break;
         case 3:
-            std::cout << "Showing list of players:\n";
             currentUser->show_list_of_players();
             break;
         case 4:
-            std::cout << "Enter player name to include: ";
-            std::cin >> new_player;
-            currentUser->include_player(new_player);
+            currentUser->include_player();
             break;
         case 5:
-            std::cout << "Enter player name to exclude: ";
-            std::cin >> new_player;
-            currentUser->exclude_player(new_player);
+            currentUser->exclude_player();
             break;
         case 6:
             std::cout << "Changing user...\n";
@@ -68,27 +65,31 @@ int main()
     return 0;
 }
 
+/**
+ * @brief Function to choose a user
+ */
 void choose_user(User*& currentUser, User& gameMaster, User& player, User& espectator)
 {
-    if(currentUser == nullptr)
+    std::cout << "Select a user:" << 
+    "\n(1) " << gameMaster.name   << 
+    "\n(2) " << player.name       << 
+    "\n(3) " << espectator.name   << "\n";
+
+    unsigned short user;
+    std::cin >> user;
+    switch(user)
     {
-        std::cout << "Select a user:\n(1) " << gameMaster.name << "\n(2) " << player.name << "\n(3) " << espectator.name << "\n";
-        unsigned short user;
-        std::cin >> user;
-        switch(user)
-        {
-            case 1:
-            currentUser = &gameMaster;
-            break;
-            case 2:
-            currentUser = &player;
-            break;
-            case 3:
-            currentUser = &espectator;
-            break;
-            default:
-            std::cout << "Invalid user.\n";
-            break;
-        };
-    }
+    case 1:
+        currentUser = &gameMaster;
+        break;
+    case 2:
+        currentUser = &player;
+        break;
+    case 3:
+        currentUser = &espectator;
+        break;
+    default:
+        std::cout << "Invalid user.\n";
+        break;
+    };
 }
